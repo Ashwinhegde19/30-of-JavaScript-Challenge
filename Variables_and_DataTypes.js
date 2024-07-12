ad004a3345369d864df7d4fec79ceae3335820a3
Ashwinhegde19 <ashwinhegde19@gmail.com>
2024-07-12 14:30:09 +0530

    Add  variable declarations and data type examples

---
+++ Variables_and_DataTypes.js
@@ -0,0 +1,60 @@
+// Variable Declaration
+var num = 56
+console.log(num)
+
+let str = "Ashwin"
+console.log(str)
+
+// Constant Declaration
+const isBoolean = true
+console.log(isBoolean)
+
+// Output:
+// 56
+// Ashwin
+// true
+
+//Data Types
+
+// Number
+let num1 = 56
+console.log(typeof num1)
+
+// String
+let str1 = "Ashwin"
+console.log(typeof str1)
+
+// Boolean
+let isBoolean1 = true
+console.log(typeof isBoolean1)
+
+// Undefined
+let a
+console.log(typeof a)
+
+// Null
+let b = null    
+console.log(typeof b)
+
+// Object
+let obj = {}
+console.log(typeof obj)
+
+// Array
+let arr = []
+console.log(typeof arr)
+
+// Function
+let func = function(){}
+console.log(typeof func)
+
+// Reassigning Variables
+let num2 = 10
+console.log(num2)
+num2 = 20
+console.log(num2)
+
+const x = 10
+x = 20        //TypeError: Assignment to constant variable.
+console.log(x)
+
